be83fe2fa6e947a771ebf4afe191a52b9fe0b7f9
SucRunBug <yyanfeiyu@163.com>
2023-05-13 17:06:32 +0800

    change dir, add some doc, write codes about build navigation and lists

---
+++ swiftUIStudy/swiftUIDemo/Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by yfy on 2023/5/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftUIStudy/swiftUIDemo/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by yfy on 2023/5/13.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: "iPhone SE (3nd generation)"))
+    }
+}
