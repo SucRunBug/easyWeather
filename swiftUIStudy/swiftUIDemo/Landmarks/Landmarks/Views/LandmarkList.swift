96d40cb37e2a0db8d831a0e7e2283e2e60e2b4c8
SucRunBug <yyanfeiyu@163.com>
2023-05-16 20:33:02 +0800

    add function of favorite filter and icon

--- swiftUIStudy/swiftUIDemo/Landmarks/Landmarks/Views/LandmarkList.swift
+++ swiftUIStudy/swiftUIDemo/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    // what is @State and why is private
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            List(landmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List{
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
@@ -24,10 +38,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        ForEach (["iPhone SE (3rd generation)", "iPhone 14 Pro"], id: \.self) {
-            deviceName in LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+        LandmarkList()
+//        ForEach (["iPhone SE (3rd generation)", "iPhone 14 Pro"], id: \.self) {
+//            deviceName in LandmarkList()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//                .previewDisplayName(deviceName)
+//        }
     }
 }
