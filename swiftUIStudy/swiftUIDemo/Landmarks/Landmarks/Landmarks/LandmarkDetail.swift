dbb1bac70a4f831124729c114d5d4ce34796c0d8
SucRunBug <yyanfeiyu@163.com>
2023-05-19 20:36:44 +0800

    draw a ugly hexagon

--- swiftUIStudy/swiftUIDemo/Landmarks/Landmarks/Landmarks/LandmarkDetail.swift
+++ swiftUIStudy/swiftUIDemo/Landmarks/Landmarks/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -21,8 +26,12 @@ struct LandmarkDetail: View {
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
+                HStack{
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                
                 
                 HStack {
                     Text(landmark.park)
@@ -48,7 +57,9 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
     static var previews: some View {
         LandmarkDetail(landmark: ModelData().landmarks[0])
+            .environmentObject(modelData)
     }
 }
